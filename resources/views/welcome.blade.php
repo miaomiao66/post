e9a7c905f7c48a77ac5342cdfa222f5f2595936e
miao <miao@miaoqi-MacBook-Pro.local>
2018-01-23 19:13:03 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    哈哈，嘻嘻嘻
                 </div>
 
                 <div class="links">
