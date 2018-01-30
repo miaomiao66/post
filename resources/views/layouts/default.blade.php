89ce0b1eb79387fafce372baa1920fbf501651aa
miao <miao@miaoqi-MacBook-Pro.local>
2018-01-30 18:13:13 +0800

    用户注册登录

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <title>@yield('title', '首页') - 苗苗博客</title>
     <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
