f50e0c3ed5f5fe3ab7024c7017473cbd652ded86
miao <miao@miaoqi-MacBook-Pro.local>
2018-01-31 16:45:02 +0800

    用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,13 +9,22 @@ use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['create', 'store', 'index']]);
+        $this->middleware('guest', ['only' => ['create']]);
+    }
+
+    // 显示用户注册页面
     public function create()
     {
         return view('users.create');
     }
 
+    // 展示用户信息
     public function show(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.show', compact('user'));
     }
 
@@ -37,5 +46,50 @@ class UsersController extends Controller
 
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
+    }
+
+    // 用户编辑页面展示
+    public function edit(User $user)
+    {
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+    }
+
+    // 编辑提交用户信息
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+        $this->authorize('update', $user);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user->id);
+    }
+
+    // 显示用户列表页面
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
+
+    // 删除用户的动作
+    public function destroy(User $user)
+    {
+        // 添加授权策略
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
     }
 }
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,12 +2,9 @@
 
 @section('content')
   <div class="jumbotron">
-    <h1>Hello Laravel</h1>
+    <h1>苗苗博客欢迎你</h1>
     <p class="lead">
-      你现在所看到的是 <a href="https://laravel-china.org/courses/laravel-essential-training-5.1">Laravel 入门教程</a> 的项目主页。
-    </p>
-    <p>
-      一切，将从这里开始。
+      您好，欢迎使用苗苗博客
     </p>
     <p>
       <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
